6b592cba2ee9d54244eef6f6549b401e671d5aa3
cuxito <sergioap28@protonmail.com>
2023-09-11 11:07:02 +0200

    a

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let pagina = document.getElementsByTagName('body');
 pagina.addEventListener('click', openFullscreen());
 
@@ -10,17 +10,4 @@ function openFullscreen() {
     } else if (pagina.msRequestFullscreen) { /* IE11 */
       pagina.msRequestFullscreen();
     }
-=======
-let pagina = document.getElementsByTagName('body');
-pagina.addEventListener('click', openFullscreen());
-
-function openFullscreen() {
-    if (pagina.requestFullscreen) {
-      pagina.requestFullscreen();
-    } else if (pagina.webkitRequestFullscreen) { /* Safari */
-      pagina.webkitRequestFullscreen();
-    } else if (pagina.msRequestFullscreen) { /* IE11 */
-      pagina.msRequestFullscreen();
-    }
->>>>>>> 1da559525e8872885c0f2451e77fd6e4702ef352
   }
