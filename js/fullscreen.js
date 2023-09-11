ecd5af9e4b7b32a4d98cc8ddeb32807185634654
cuxito <sergioap28@protonmail.com>
2023-09-11 15:00:21 +0200

    s

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,13 +1,5 @@
 
 let pagina = document.getElementsByTagName('body');
-pagina.addEventListener('click', openFullscreen());
-
-function openFullscreen() {
-    if (pagina.requestFullscreen) {
-      pagina.requestFullscreen();
-    } else if (pagina.webkitRequestFullscreen) { /* Safari */
-      pagina.webkitRequestFullscreen();
-    } else if (pagina.msRequestFullscreen) { /* IE11 */
-      pagina.msRequestFullscreen();
-    }
-  }
+pagina.addEventListener('click', (event)=>{
+    console.log(event);
+});
