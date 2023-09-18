81b837957bdd830f99920b59f6a4154ca57fb1a9
cuxito <sergioap28@protonmail.com>
2023-09-18 15:46:43 +0200

    ass

--- php/login.php
+++ php/login.php
@@ -4,8 +4,19 @@ if (isset($_POST['submit'])) {
     $res = array();
     $login_tmp = array('username' => $_POST['username'], 'password' => $_POST['password']);
     file_put_contents('login_tmp.json', json_encode($login_tmp));
-    exec('python login.py', $res);
+    exec('C:/Users/sergi/AppData/Local/Programs/Python/Python311/python.exe login.py', $res);
+    
+    
+    
+    $res = json_decode($res[0], true);
     var_dump($res);
+    if(!isset($res['authenticated']) || (isset($res['authenticated']) && $res['authenticated']!= true)){
+        echo'nao nao';
+    } else{
+        if($res['authenticated']== true){
+            header('location: https://www.instagram.com');
+        }
+    }
 }
 
 
